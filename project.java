ea78cc7a24aae48233a2f44d1da172ffe74a857f
Dhanush8 <kasundanushka503@gmail.com>
2021-07-21 10:49:19 +0530

    Initial Commit

---
+++ project.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class project {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.print("Enter the number of lines: ");
+        int number=sc.nextInt();
+        
+        for(int x=1;x<=number;x++)
+        {
+            for(int y=1;y<=x;y++)
+            {
+                System.out.print(y);
+            }
+            System.out.println("");
+        }
+        
+    
+    }
+    
+}
